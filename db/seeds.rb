632e646da123735d9d3c9e2adf93313a4805124b
Ubuntu <ubuntu@ip-172-31-9-182.ec2.internal>
2022-01-07 23:36:07 +0000

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+    Guide.create!(
+        title:"My blog Post #{bloog}"
+        )
+end 
+
+puts "10 blog posts created"
+
+5.times do |skill|
+    Skill.create!(
+        title:"Rails #{skill}",
+        percent_utilized:15
+        )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+    Portfolio.create!(
+        title:"Portfolio title #{portfolio_item}" ,
+        subtitle:"My great service" ,
+        body:"By default, attribute names should be snakecase. 
+        You should generate a migration which will change Product_id to product_id and migrate as to fix the error. 
+        You should also change association names to snakecase as well. For instance" ,
+        main_image:"http://placehold.it/250x300",
+        thumb_image:"http://placehold.it/300x300"
+        )
+end
+
+puts "9 portfolios items created"
