5f64a02d15e7b9d89075ea856eb2d83e9b6109da
Ubuntu <ubuntu@ip-172-31-9-182.ec2.internal>
2022-01-10 22:52:56 +0000

    Implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -20,5 +20,23 @@ class PortfoliosController < ApplicationController
           end
         end
     end
+    
+    def edit
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+  
+    def update
+        @portfolio_item = Portfolio.find(params[:id])
+        
+        respond_to do |format|
+          if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle))
+            format.html { redirect_to portfolios_path, notice: "The record was successfully updated." }
+            format.json { render :show, status: :ok, location: @portfolio_item }
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+            format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+          end
+        end
+    end
   
 end
