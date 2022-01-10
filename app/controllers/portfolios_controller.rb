118adb63c458ac8dc7cac0477137140c18069a52
Ubuntu <ubuntu@ip-172-31-9-182.ec2.internal>
2022-01-10 22:17:21 +0000

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,24 @@
 class PortfoliosController < ApplicationController
     def index
-        @portfolios_items = Portfolio.all
+        @portfolio_items = Portfolio.all
     end
+    
+    def new
+        @portfolio_item = Portfolio.new
+    end  
+    
+    def create
+        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle))
+
+        respond_to do |format|
+          if @portfolio_item.save
+            format.html { redirect_to portfolios_path, notice: "You portfolio item is now live." }
+            format.json { render :show, status: :created, location: @portfolio_item }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+  
 end
