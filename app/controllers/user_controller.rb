fad48d4b7c788720cfa0743b8bbdea0455c7727d
Ubuntu <ubuntu@ip-172-31-9-182.ec2.internal>
2022-02-08 00:49:35 +0000

    Activity Message part 6

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -20,41 +20,8 @@ class UserController < ApplicationController
       end
   end
     
-  def edit
-       @user = User.find(params[:id])
-  end  
-  
-  def update
-        @user = User.find(params[:id])
-        
-        respond_to do |format|
-          if @user.update(params.require(:message).permit(:body, :active))
-            format.html { redirect_to messages_path, notice: "The record was successfully updated." }
-            format.json { render :show, status: :ok, location: @user }
-          else
-            format.html { render :edit, status: :unprocessable_entity }
-            format.json { render json: @user.errors, status: :unprocessable_entity }
-          end
-        end
-  end
-  
-    
   def show
       @user = User.find(params[:id])
-  end
-  
-  def destroy
-      # Perform the lookup
-      @user = User.find(params[:id])
-       
-       # Destroy/Delete the record
-      @user.destroy
-      
-      # Redirect
-      respond_to do |format|
-        format.html { redirect_to user_url, notice: "The record was removed." }
-     
-      end
   end
 
 end
