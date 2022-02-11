9773b579436bad10b7ffcf2720bcab87505bc65f
Ubuntu <ubuntu@ip-172-31-9-182.ec2.internal>
2022-02-11 21:45:56 +0000

    Customized registration, login, and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :ussers
+  devise_for :ussers, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as:'portfolio_show'
