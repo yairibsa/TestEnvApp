dc47e2fc73e44bbb46b81ce7ea9d43cb922d5396
Ubuntu <ubuntu@ip-172-31-9-182.ec2.internal>
2022-02-17 23:05:35 +0000

    Implemented view helper for auth links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def login_helper
+     if current_usser.is_a?(Usser) 
+       link_to "Logout", destroy_usser_session_path, method: :delete
+     else 
+       (link_to "Register", new_usser_registration_path) +
+       "<br>".html_safe +
+       (link_to "Login", new_usser_session_path) 
+     end 
+    end
 end
