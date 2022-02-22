f48984ff5b45ec93711002f28ff571a217699144
Ubuntu <ubuntu@ip-172-31-9-182.ec2.internal>
2022-02-22 20:45:33 +0000

    Fixed validates for usser model

--- app/models/usser.rb
+++ app/models/usser.rb
@@ -3,11 +3,23 @@ class Usser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  validates_presence_of :name
+  
   def first_name
-    self.name.split.first
+    self.name.split.first.oops
   end
   
   def last_name
     self.name.split.last
+  end
+  
+  def get_facebook_messages
+    begin
+      contacts_fb
+      @messages = retrieves_messages
+    rescue => e
+      flash[:error] = "Error ocurred contacting Facebook: #{e}"
+    end
   end
 end
