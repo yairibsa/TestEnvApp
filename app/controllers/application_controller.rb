e77ded12cc47c97c7a94b88543bca1db2f64134f
Ubuntu <ubuntu@ip-172-31-9-182.ec2.internal>
2022-02-23 21:59:44 +0000

    Implemented copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,18 @@ class ApplicationController < ActionController::Base
   include CurrentUsserConcern
   include DefaultPageContent
   
+  before_action :set_copyright
+  
+  def set_copyright
+    @copyright = TestEnvViewTool::Renderer.copyright 'Hugo Ibañez', 'All rights reserved'
+  end
+  
+end
+
+module TestEnvViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}<b> #{msg}".html_safe
+    end
+  end
 end
