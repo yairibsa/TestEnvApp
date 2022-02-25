4ca682782b3c09db9be8f63a58f47377a2da74e4
Ubuntu <ubuntu@ip-172-31-9-182.ec2.internal>
2022-02-25 22:28:13 +0000

    Updated master layout styles along with helper methods

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as:'portfolio_show'
   
-  get 'about', to: 'pages#about'
+  get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   
   resources :guides do
