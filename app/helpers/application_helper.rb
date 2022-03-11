a48c9a0a59b12746b4758072e4664b4f544ceacc
Ubuntu <ubuntu@ip-172-31-9-182.ec2.internal>
2022-03-11 23:52:07 +0000

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -55,4 +55,16 @@ module ApplicationHelper
         "active" if current_page? path
     end
     
+    def alerts
+        alert = (flash[:alert] || flash[:error] || flash[:notice])
+        
+        if alert 
+            alert_generator alert
+        end
+    end
+    
+    def alert_generator msg
+        js add_gritter(msg, title: "Hugo Ibañez Portfolio", sticky: false)
+    end
+    
 end
