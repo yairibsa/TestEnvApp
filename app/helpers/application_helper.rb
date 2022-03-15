dce9cc29423b5dedddd33b4c632eb0a8698a98e5
Ubuntu <ubuntu@ip-172-31-9-182.ec2.internal>
2022-03-15 23:06:10 +0000

    implemented custom style tweets for tech-news

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,6 +37,10 @@ module ApplicationHelper
             {
                 url: portfolios_path,
                 title: 'Portfolios'
+            },
+            {
+                url: tech_news_path,
+                title: 'Tech News'
             }
         ]
     end
