771ec6c243637d147dbdc17a91aa558adf03ca7e
Ubuntu <ubuntu@ip-172-31-9-182.ec2.internal>
2022-03-16 23:41:02 +0000

    Created a comment broadcast, configured the websocket route, and updated the comment model with a callback

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :guide
   
   validates :content, presence: true, length: { minimum: 5, maximimum: 100}
+  
+  after_create_commit { CommentBroadcastJob.perfom_later(self) }
 end
