95c3d8c9ca1441e2a038bc6c9ce84bb8135b6de5
Ubuntu <ubuntu@ip-172-31-9-182.ec2.internal>
2022-03-25 23:46:11 +0000

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+    email: "test@email.com",
+    password: "123456",
+    password_confirmation: "123456",
+    name: "Admin User",
+    roles: "site_admin"
+    )
+puts "1 Admin user created"
+
+User.create!(
+    email: "test2@email.com",
+    password: "123456",
+    password_confirmation: "123456",
+    name: "Regular User"
+    )
+puts "1 regular user created"
+
 3.times do |topic|
     Topic.create!(
         title: "Topic #{topic}")
@@ -15,6 +32,9 @@ puts "3 Topics created"
 10.times do |blog|
     Guide.create!(
         title:"My blog Post #{blog}",
+        body: "By default, attribute names should be snakecase. 
+        You should generate a migration which will change Product_id to product_id and migrate as to fix the error. 
+        You should also change association names to snakecase as well. For instance",
         topic_id: Topic.last.id
         )
 end 
